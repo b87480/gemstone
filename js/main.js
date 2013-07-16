0e93ef7cd0d75eab8d28e441902dc967f7c98234
Joshua Whitney <jwhitney114@fullsail.edu>
2013-07-16 11:31:15 -0400

    New stuff

--- js/main.js
+++ js/main.js
@@ -10,8 +10,17 @@ $('#read2').click(function(){
 	StopMusic();
 });
 
+$('#ch1').on('pageinit', function(){ //start ch1 page
+	$('#ch1img').css({'position': 'relative', 'right': '-100px', 'top': '-150px', 'width': '10%', 'z-index': '1000'});
 
 
+$('#ch1img').click(function(){
+	$('#ch1img').effect("bounce", 'slow');
+	playBlip();
+
+});
+
+});//end ch1 page
 
 });
 
@@ -25,4 +34,8 @@ function StopMusic(){
 	var sound = document.getElementById('music');
 	sound.pause();
 };
+function playBlip(){
+	var sound = document.getElementById('blip');
+	sound.play();
+}
 
